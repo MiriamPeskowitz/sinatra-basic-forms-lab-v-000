5e36bc41b8f91818c9b83f87ebd2e8300f96eaeb
Miriam Peskowitz <miriam5000@gmail.com>
2017-11-17 18:09:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   #      "Hello World"
   #    end
 
-   post '/new' do
+   post '/puppy' do
     @puppy = Puppy.new(params[:name], params[:breed], params[:age])
     erb  :display_puppy
   end
